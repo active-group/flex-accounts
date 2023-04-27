b8c0e96d2cd776782343ea512224a09ba645d10f
Ralph Riedel <ralph.riedel@protonmail.com>
2023-04-27 15:06:15 +0200

    Weitere Fixes

--- src/server.erl
+++ src/server.erl
@@ -2,7 +2,7 @@
 
 -behaviour(gen_server).
 -include("events.hrl").
--export([init/1, start/1, handle_call/3, handle_cast/2, publish/2]).
+-export([init/1, start/1, handle_call/3, handle_cast/2, publish/2, handle_info/2]).
 
 -type message() :: {pid(), no_events} | {pid(), #event{}}  | #event{}.
 -type state() :: list(pid()).
@@ -16,9 +16,11 @@ init(PidList) ->
     {ok, PidList}.
 
 -spec handle_call(message(), gen_server:from(), state()) -> {reply, {ok, #event{}}, state()}.
-handle_call({Pid, no_events}, _From, List) -> 
+handle_call({Pid, no_events}, _From, List) ->
+    erlang:monitor(process, Pid), 
     {reply, {ok, database:get_all_events()}, lists:uniq([Pid | List])};
 handle_call({Pid, Event}, _From, List) -> 
+    erlang:monitor(process, Pid),
     {reply, {ok, database:get_events_from(Event#event.number)}, lists:uniq([Pid | List])}.
 
 %handle_call(Message, _From, List) -> 
@@ -38,3 +40,9 @@ publish(Event, [First | Rest]) ->
     io:format("Casting to ~w: Event=~w~n", [First, Event]),
     gen_server:cast(First, Event),
     publish(Event, Rest).
+
+-spec handle_info(message(), state()) -> {noreply, state()}.
+handle_info({'DOWN', _Ref, process, Pid2, Reason}, PidList) ->
+    io:format("Subscriber ~w removed: ~w~n", [Pid2, Reason]),
+    {noreply, lists:delete(Pid2, PidList)}.
+
