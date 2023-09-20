8f56f52186812bf4545b06d80d71b69fc481b2d1
Ferenc Noack <ferenc.noack@gematik.de>
2023-09-20 10:00:24 +0200

    print_all_accounts

--- src/client.erl
+++ src/client.erl
@@ -2,7 +2,7 @@
 
 -module(client).
 -include("data.hrl").
--export([open_account/2]).
+-export([open_account/2, print_all_accounts/0]).
 
 %% opens an acocunt with a given name and surname.
 %% prints the result and the account number to stdout.
@@ -11,3 +11,10 @@ open_account(GivenName, Surname) ->
     Account = business_logic:open_account(list_to_binary(GivenName),
                                           list_to_binary(Surname)),
     io:format("Account was successfully opened. Account number: ~p ~n", [Account#account.account_number]).
+
+-spec print_all_accounts() -> ok.
+print_all_accounts() ->
+  Accounts = database:get_all_accounts(),
+   lists:foreach(
+     fun(#account{account_number = Account_number, person_id = Person_id, amount = Amount}) ->
+       io:format("~p - ~p - ~p~n", [Account_number, Person_id, Amount]) end, Accounts).
