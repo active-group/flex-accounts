825a11da2902709d9cc76fae14d09a634366353d
Jens Frahne <jens.frahne@materna.group>
2024-06-12 13:50:55 +0200

    get_person / get_account in client api

--- src/client.erl
+++ src/client.erl
@@ -2,7 +2,7 @@
 
 -module(client).
 -include("data.hrl").
--export([open_account/2]).
+-export([open_account/2,get_person/1,get_account/1]).
 
 
 %% opens an acocunt with a given name and surname.
@@ -12,4 +12,11 @@ open_account(GivenName, Surname) ->
     Account = business_logic:open_account(list_to_binary(GivenName),
                                           list_to_binary(Surname)),
     io:format("Account was successfully opened. Account number: ~p ~n", [Account#account.account_number]).
+
+-spec get_person(unique_id()) -> {ok, #person{} | {error, any()}}.
+get_person(Id) -> business_logic:get_person(Id).
+
+
+-spec get_account(account_number()) -> {ok, #account{}} | {error, any()}.
+get_account(AccountNumber) -> business_logic:get_account(AccountNumber).
 
