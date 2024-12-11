b53e39dd3025e47701772de7fc10f47c61765fbf
„Ulrich <ulrich.beez@cofinpro.de>
2024-12-11 16:38:10 +0100

    .

--- src/account_server.erl
+++ src/account_server.erl
@@ -1,19 +1,19 @@
 
 %Annahme: Subscription kann z.B. alle 30s wiederholt werden. 
 %
-%Subscription handling
-%mit parameter 'ich habe die konten bis kontonumer X' + Subscriber PID
-% 1. Fall X kleiner current:
-% Antwort: delta(x bis current).
-% 2. Fall X >= current:
+% Subscription handling
+%   -> mit parameter 'ich habe die Accounts bis Accountnumber X' + Subscriber PID
+% 1. Fall X kleiner Current-Accountnumber:
+% Antwort: delta(X bis Current-Accountnumber).
+% 2. Fall X >= Current-Accountnumber:
 % Antwort: keine.
 
 % Aliveness-Problematik 
 % Bei 1 u. 2
-% Merken von Subscriber PID für in Liste
+% Merken von Subscriber-PID in Liste.
 % A. Wenn PID bereits bekannt, keine aufnahme ins Monitoring aber antworten wie bei 1. 
 % B. Wenn PID nicht bekannt, aufnahme ins Monitoring und antworten wie bei 1. 
-% Bei Monitor-Fail: entfernen aus PID Liste
+% Bei Monitor-Fail: entfernen aus PID-Liste.
 
 % Mitbekommen von Account-Neuanlagen
 % Bei Accountneuanlage: Antwort an alle in PID-Liste mit neuem Account.
