07bd42eae4cc54bdf0fc15d27e0d93711367156b
VMT-GMBH\P.Voigt <p.voigt@vmt-gmbh.de>
2024-12-12 11:29:11 +0100

    @work

--- src/business_logic.erl
+++ src/business_logic.erl
@@ -18,16 +18,30 @@
     amount :: money()
 }).
 
+-record(account_dto, {
+    account_number :: number(),
+    person_id :: number(),
+    given_name :: string(),
+    surname :: string(),
+    amount :: number()
+}).
+
 %% Opens an account, that is creates a new account containing a new person
 %% Writes them into database.
 
 -spec open_account(binary(), binary()) -> #account{}.
 open_account(GivenName, Surname) ->
-    make_account(
-        make_person(
-            GivenName, Surname
-        )
-    ).
+    Person = make_person(GivenName, Surname),
+    Account = make_account(Person),
+    AccountDto = #account_dto{
+        account_number = Account#account.account_number,
+        person_id = Account#account.person_id,
+        given_name = Person#person.given_name,
+        surname = Person#person.surname,
+        amount = Account#account.amount
+    },
+    account_server:add_account(AccountDto),
+    Account.
 
 -spec get_account(account_number()) -> {ok, #account{}} | {error, any()}.
 get_account(AccountNumber) -> database:get_account(AccountNumber).
