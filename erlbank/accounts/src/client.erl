139c2276884db8020015ff753ac8bbf87eba7dad
selencaca <scaca@oev.de>
2025-12-02 16:39:44 +0100

    initialize create_account module

---
+++ erlbank/accounts/src/client.erl
@@ -0,0 +1,14 @@
+%% This module represents the frontend layer
+
+-module(client).
+-include("data.hrl").
+-export([open_account/2]).
+
+
+%% opens an acocunt with a given name and surname.
+%% prints the result and the account number to stdout.
+-spec open_account(string(), string()) -> ok.
+open_account(GivenName, Surname) ->
+    Account = business_logic:open_account(list_to_binary(GivenName),
+                                          list_to_binary(Surname)),
+    io:format("Account was successfully opened. Account number: ~p ~n", [Account#account.account_number]).
---
+++ erlbank/accounts/src/data.hrl
@@ -0,0 +1,13 @@
+-type unique_id() :: integer().
+-type account_number() :: integer().
+-type money() :: number().
+
+-record(person, 
+    {id :: unique_id(), 
+     given_name :: binary(),
+     surname :: binary()}).
+-record(account,
+    {account_number :: account_number(),
+     person_id :: unique_id(),
+     amount :: money()}).
+
