3a2e676cd625b9cf0bcf9f066edf101e5fbf466f
selencaca <scaca@oev.de>
2025-12-02 16:43:37 +0100

    remove transaction related tests

--- erlbank/accounts/test/database_test.erl
+++ erlbank/accounts/test/database_test.erl
@@ -12,7 +12,7 @@ main_test_() ->
      {foreach,
       fun setup/0,
       fun cleanup/1,
-      [fun put_person/1, fun put_account/1, fun put_transfer/1]
+      [fun put_person/1, fun put_account/1]
      }}.
 
 put_person(_) ->
@@ -29,16 +29,6 @@ put_account(_) ->
             database:put_account(Account),
             ?assertEqual(database:get_account(42), {ok, Account}),
             ?assertEqual(database:get_all_accounts(), [Account])
-    end.
-
-put_transfer(_) ->
-    fun() ->
-            Transfer = #transfer{id = 17, timestamp = {1610,547469,326863}, from_account_number = 17, to_account_number = 32, amount = 100 },
-            database:put_transfer(Transfer),
-            ?assertEqual(database:get_transfer(17), {ok, Transfer}),
-            ?assertEqual(database:get_all_transfers(), [Transfer]),
-            ?assertEqual(database:get_all_transfers(17), [Transfer]),
-            ?assertEqual(database:get_all_transfers(16), [])
     end.
 
 
