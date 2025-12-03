5c7a4a857f87b5e95fd77d6d628465b1be93de60
selencaca <scaca@oev.de>
2025-12-03 15:13:09 +0100

    implement events_server

--- erlbank/accounts/src/data.hrl
+++ erlbank/accounts/src/data.hrl
@@ -11,3 +11,18 @@
      person_id :: unique_id(),
      amount :: money()}).
 
+-record(person_creation_event,
+{id :: unique_id(),
+  given_name :: binary(),
+  surname :: binary()}).
+
+-record(account_creation_event,
+{account_number :: account_number(),
+  person_id :: unique_id()}).
+
+-record(transfer_creation_event,
+{id :: unique_id(),
+  timestamp :: erlang:timestamp(),
+  from_account_number :: account_number(),
+  to_account_number :: account_number(),
+  amount :: money()}).
